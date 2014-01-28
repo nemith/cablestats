9052c07da14b252bbac345c9ffbdd618848a0ed2
Brandon Bennett <bennetb@gmail.com>
2014-01-28 16:09:01 -0700

    PEP8 setup.py too

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 from distutils.core import setup
 
 setup(
-    name = 'cablestats',
-    version = "0.1.1",
-    description = 'Hacky hack to get cable stats.  Includes munin plugins.',
-    author = 'Brandon Bennett',
-    author_email = 'bennetb@gmail.com',
-    packages = ['cablestats'],
-    install_requires = [
-    	'requests',
-    	'lxml',
+    name='cablestats',
+    version="0.1.1",
+    description='Hacky hack to get cable stats.  Includes munin plugins.',
+    author='Brandon Bennett',
+    author_email='bennetb@gmail.com',
+    packages=['cablestats'],
+    install_requires=[
+        'requests',
+        'lxml',
     ],
-    dependency_links = [
-    	'git+https://github.com/samuel/python-munin',
+    dependency_links=[
+        'git+https://github.com/samuel/python-munin',
     ],
-    data_files = [
+    data_files=[
         ('share/cablestats/munin_plugins',
             ['munin_plugins/cablemodem_ds_power',
              'munin_plugins/cablemodem_ds_snr',
