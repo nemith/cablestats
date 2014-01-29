74d98ea759fd93ca45a90fe78b8e336b9f1bb740
Brandon Bennett <bennetb@gmail.com>
2014-01-28 18:44:09 -0700

    Add critical and warning limits to munin plugins

--- cablestats/munin_helper.py
+++ cablestats/munin_helper.py
@@ -10,6 +10,8 @@ def _to_long_name(ch):
 
 class CableModemPlugin(MuninPlugin):
     category = "Cable"
+    warning = None
+    critical = None
 
     def __init__(self):
         self.host_name = os.environ.get("CM_HOST", "10.1.10.1")
@@ -27,12 +29,18 @@ class CableModemPlugin(MuninPlugin):
     def fields(self):
         ret = []
         for ch in self.data.iterkeys():
-            field = (ch, dict(
+            data = dict(
                 label=_to_long_name(ch),
                 type="GAUGE",
                 info=self.channel_info(ch)
-            ))
-            ret.append(field)
+            )
+
+            for k in ('warning', 'critical'):
+                v = getattr(self, k, None)
+                if v is not None:
+                    data[k] = v
+
+            ret.append((ch, data))
         return ret
 
     def execute(self):
